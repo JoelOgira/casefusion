eed1f64e11f8dd786518aedf50031ad06537980b
Jay <joelogira@gmail.com>
2024-07-11 11:15:37 +0300

    Added review and footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
+import { Icons } from '@/components/Icons'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Phone from '@/components/Phone'
-import { Check, Star } from 'lucide-react'
+import { Reviews } from '@/components/Reviews'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { ArrowRight, Check, Star } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -107,6 +112,172 @@ export default function Home() {
               />
             </div>
           </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* Value Proposition Section */}
+      <section className='bg-slate-100 py-24'>
+        <MaxWidthWrapper
+          className='flex flex-col items-center gap-16 sm:gap-32'
+        >
+          <div className="flex flex-col items-center gap-4 sm:gap-6 lg:flex-row">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl text-gray-900 md:text-6xl">
+              What Our{' '}
+              <span className="relative px-2">
+                Customers <Icons.underline className='hidden pointer-events-none absolute inset-x-0 text-green-500 -bottom-6 sm:block' />
+              </span>{' '}
+              Say
+            </h2>
+
+            <img
+              src="/snake-2.png"
+              className='w-24 order-0 italic lg:order-2'
+              alt="snake-2 image"
+            />
+
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex mb-2 gap-0.5">
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;The case feels durable and I even got a compliment on the design. Had the case for two and a half months and <span className='p-0.5 bg-slate-800 text-white' >the image is super clear</span>, on the case I had before, the image started fading into yellow-ish color after a couple of weeks. Love it.&quot;
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-1.png"
+                  alt="User 1"
+                  className='rounded-full size-12 object-cover italic'
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Anton Wajockastri</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className='size-4 stroke-[3px] text-green-600' />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+
+            {/* Second Review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex mb-2 gap-0.5">
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+                <Star className='size-5 text-green-600 fill-green-600' />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;My other used to scratch so easily which would remain permanently.  Casefusion barely has scratches after half a year.<span className='p-0.5 bg-slate-800 text-white' >Looks good as new</span>. I definitely recommend it!&quot;
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-4.jpg"
+                  alt="User 2"
+                  className='rounded-full size-12 object-cover italic'
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Malik Bonaparte</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className='size-4 stroke-[3px] text-green-600' />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+
+      <section className="">
+        <MaxWidthWrapper
+          className='py-24'
+        >
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl text-gray-900 md:text-6xl">
+                Upload your photo and get{' '}
+                <span className="relative px-2 bg-green-600 text-white">
+                  your own case
+                </span>{' '}
+                now.
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0 italic"
+                alt='arrow image'
+              />
+
+              <div className="relative h-80 md:size-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img src="/horse.jpg" alt="horse image" className="italic rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 size-full" />
+              </div>
+
+              <Phone
+                imgSrc='/horse_phone.jpg'
+                className='w-60'
+              />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className='size-5 text-green-600 inline mr-1.5' />
+              High-quality Silicone material
+            </li>
+            <li className="w-fit">
+              <Check className='size-5 text-green-600 inline mr-1.5' />
+              Scratch and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className='size-5 text-green-600 inline mr-1.5' />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className='size-5 text-green-600 inline mr-1.5' />
+              5 year warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                href="/configure/upload"
+                className={cn(buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8"
+                }))}
+              >
+                Create your case now <ArrowRight className='size-4 ml-1.5' />
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </main>
